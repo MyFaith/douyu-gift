31074569fd7d1ea679be5c5e5c84f5f94bf1a18b
Faith <854372534@qq.com>
2025-07-05 11:32:11 +0800

    Update message.ts

--- src/common/message.ts
+++ src/common/message.ts
@@ -18,30 +18,16 @@ async function sendMessage() {
     text: `【斗鱼荧光棒-完成】`,
     desp: fs.readFileSync(path.join(__dirname, "..", "..", "douyu.log"), "utf-8")
   };
-  if (data.desp) {
-    logger.info("------执行推送------");
-    await axios({
-      method: "post",
-      url,
-      data,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    });
-    logger.info("------推送成功------");
-  } else {
-    await axios({
-      method: "post",
-      url,
-      data: {
-        text: "斗鱼荧光棒-错误",
-        desp: "执行出现问题,日志为空"
-      },
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    });
-  }
+  logger.info("------执行推送------");
+  await axios({
+    method: "post",
+    url,
+    data,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
+  logger.info("------推送成功------");
 }
 
 export default sendMessage;
